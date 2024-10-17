54809e03f3c88e11f0a1518dd37859e3084d7452
Gavin <gavin@litenlite.com>
2024-10-17 16:26:56 -0700

    added ios project

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Gavin Donoho on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Scrumdinger/ScrumdingerTests/ScrumdingerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerTests.swift
+//  ScrumdingerTests
+//
+//  Created by Gavin Donoho on 10/17/24.
+//
+
+import Testing
+@testable import Scrumdinger
+
+struct ScrumdingerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
